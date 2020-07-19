eb5df361cf176f939729eeb2a32ffe5821f04be1
Nikita Ulko <ulkenauer@gmail.com>
2020-07-19 20:09:39 +0700

    CLR-S-1 fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
 import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
+import { SessionService } from './services/session/session.service';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController, AuthController],
-  providers: [AppService, UserService, AuthService],
+  providers: [AppService, UserService, AuthService, SessionService],
 })
 export class AppModule {}
